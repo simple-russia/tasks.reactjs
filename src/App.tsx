c95dd4081fff4fea205836a21e221fb508a91849
DXaisaev <alexeyisaev29032001@gmail.com>
2023-01-05 21:39:42 +0300

    finished basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,12 @@
-import logo from './logo.svg';
-import { ReactComponent as Al } from './logo.svg';
-import './App.css';
-import { useEffect } from 'react';
 import { observer } from 'mobx-react-lite';
-import { appStore } from './store';
+import './global.css';
 
 
 
 const App = observer(() => {
-    useEffect(() => {
-        console.log('mounted');
-    }, []);
-
     return (
-        <div className="App">
-            <header className="App-header">
-                counter: {appStore.counter} <button onClick={() => appStore.increment()}>+1</button>
-                <Al className="App-logo" fill='red' />
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        <div>
+            App
         </div>
     );
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import { App } from './App';
 
 const root = ReactDOM.createRoot(
@@ -8,7 +6,5 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+    <App />
 );
