d447708b6ba6d93035770998f6e167f7b6610c18
DXaisaev <alexeyisaev29032001@gmail.com>
2023-01-08 04:40:43 +0300

    top level import tsconfig

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { observer } from 'mobx-react-lite';
 import { useEffect } from 'react';
-import { authStore } from './stores/authStore';
-import './global.css';
-import { AppRouter } from './components/appRouter/appRouter';
 import { BrowserRouter } from 'react-router-dom';
-import { AppLayout } from './components/appLayout/appLayout';
+
+import { authStore } from 'stores/authStore';
+
+import { AppRouter } from 'components/appRouter/appRouter';
+import { AppLayout } from 'components/appLayout/appLayout';
+
+import './global.css';
 
 
 
