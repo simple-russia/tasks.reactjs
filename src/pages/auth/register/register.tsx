ec0266639802a2617a31ebd72525883d701efd41
DXaisaev <77843094+simple-russia@users.noreply.github.com>
2023-01-15 03:31:43 +0300

    register base

--- src/pages/auth/register/register.tsx
+++ src/pages/auth/register/register.tsx
@@ -2,7 +2,8 @@ import { authStore } from 'stores/authStore';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { observer } from 'mobx-react-lite';
-
+import styles from './register.module.scss';
+import { TitleBar, Wrapper3d } from 'components/ui/decoration';
 
 
 export const Register = observer(() => {
@@ -17,6 +18,16 @@ export const Register = observer(() => {
 
 
     return (
-        <div>Register</div>
+        <div className={styles.register_page_cont}>
+            <Wrapper3d className={styles.register_wrapper} offset={12} lineWidth={1}>
+                <div className={styles.register_box}>
+                    <TitleBar className={styles.top_bar} />
+                    <div className={styles.image_box}></div>
+                    <div className={styles.form_box}>
+                        <div style={{ height: '100%', width: '100%', background: '#FFF' }}></div>
+                    </div>
+                </div>
+            </Wrapper3d>
+        </div>
     );
 });
